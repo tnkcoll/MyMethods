8379f4e19c0aa2845aef1cd720fe6d91f7bff4c9
tnkcoll <theodore.collins2023@outlook.com>
2024-06-17 09:07:39 -0500

    Add project files.

---
+++ MyMethods.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMethods
+{
+    internal class MyMethods
+    {
+       public void PrintArray(int[] ints)
+        {
+            foreach (int i in ints)
+            {
+                //Print the array passed into this method.
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
